9f27eb50121ba2ffd81a3789f4c40fce46db1722
KyoZeus <franciscopaivaknebel@gmail.com>
2016-07-30 03:14:18 -0300

    Added video background.

--- server/routes/static.js
+++ server/routes/static.js
@@ -4,6 +4,8 @@ module.exports = function (app, dirname) {
 	app.use('/css', express.static(dirname + '/client/css'));
 	app.use('/js', express.static(dirname + '/client/js'));
 	app.use('/img', express.static(dirname + '/client/img'));
+	app.use('/video', express.static(dirname + '/client/video'));
+
 
 	app.use('/js', express.static(dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 	app.use('/js', express.static(dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
